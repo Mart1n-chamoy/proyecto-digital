c0843d1222092abe87492b5296042e9817e587d2
Mart1n chamoy <2004colussi@gmail.com>
2026-08-01 19:44:56 -0300

    database added

--- proyecto/gestor/urls.py
+++ proyecto/gestor/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.saludo, name='saludo'),
-    path('inicio/', views.inicio, name='inicio'),
+    path('', views.inicio, name='inicio'),
+    path('documentos/', views.documentos, name='documentos'),
+    path('documentos/crear/', views.crear_documento, name='crear'),
+    path('documentos/editar/', views.editar_documento, name='editar'),
 ]
--- proyecto/gestor/views.py
+++ proyecto/gestor/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def saludo(request):
-    return HttpResponse("<h1>Bienvenido a la página del gestor.</h1>")
 def inicio(request):
     return render(request, 'paginas/inicio.html')
+def documentos(request):
+    return render(request, 'documentos/index.html')
+def crear_documento(request):
+    return render(request, 'documentos/crear.html')
+def editar_documento(request):
+    return render(request, 'documentos/editar.html')
